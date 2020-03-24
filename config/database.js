39dc9efe949b0c06b2af9bf448b4159dd87e4787
Anjelica Bethea <anjelicabethea@gmail.com>
2020-03-23 21:56:57 -0400

    starter

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://anjelica:123@cluster0-qmt3m.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
